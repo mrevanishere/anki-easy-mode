7d6ba66af7e34b242ad925633176ca42427c60e8
mrevanisworking <evanjoshbui@gmail.com>
2020-10-09 14:22:58 -0700

    custom sorting of card queue working

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@
 
 import sys
 import os
-
+import random
 import anki
 
 from aqt.qt import *
@@ -14,13 +14,9 @@ from aqt.utils import tooltip
 
 from anki.sched import Scheduler
 from anki.schedv2 import Scheduler
-from anki.utils import ids2str
 
 from anki.hooks import addHook, wrap
-
-
-# from anki.sound import play
-# from anki.lang import _
+from . import sortconfig
 
 
 def setup_ui(self, Dialog):
@@ -28,77 +24,77 @@ def setup_ui(self, Dialog):
 
     grid = QGridLayout()
     self.toggleAddon = QCheckBox("Enable Anki Ultimate")
-    self.toggleAddon.stateChanged.connect(checked)
+    # self.toggleAddon.stateChanged.connect(checked)
     grid.addWidget(self.toggleAddon, 0, 0, 1, 1)
     self.verticalLayout_6.insertLayout(4, grid)
 
 
-def fill_review(self, _old):
+def fill_review(self, recursing=False) -> bool:
+    "True if a review card can be fetched."
     if self._revQueue:
         return True
     if not self.revCount:
         return False
-    self._revQueue = self.col.db.list("""select id from cards order by ivl and queue = 2""")
-    # if self.col.decks.current()['dyn']:
-    #     run_tests.state = 3
-    #     return _old(self)
-    # did = self._revDids[0]
-    # d = self.col.decks.get(self.col.decks.selected(), default=False)
-    # lim = min(self.queueLimit, currentRevLimit(self))
-    # lim = min(self.queueLimit, currentRevLimit(self))
-    # lim = 200
-    # if lim:
-        # sort_by = SORT_CHOICE
-        # sort_by = "lapses asc, " \
-        #           "(select max(revlog.ease) from revlog where revlog.cid = cards.id) desc," \
-        #           "(select max(revlog.time) from revlog where revlog.cid = cards.id) asc "
-        # sort_by1 = "order by lapses asc"
-        # sql = """
-        # select id from cards where did in ?
-        # order by %s and queue = 2 and due <= ? limit ?
-        # """
-        # perDeckLimit ?
-        # prioritize today?
 
-        # deck_list = ids2str(self.col.decks.active())
+    anki_ultimate = 1
+    if not anki_ultimate:
+        return False
 
-        # self._revQueue = self.col.db.list(sql, did, sort_by1, self.today, lim)
-        # self._revQueue = self.col.db.list("""select id from cards where did in %s
-        # and queue = 2 limit ?""" % ((deck_list, sort_by1), lim))
+    while self._revDids:
+        did = self._revDids[0]
+        lim = min(self.queueLimit, self._deckRevLimit(did))
+        if lim:
+            # fill the queue with the current did
 
-        # self._revQueue = self.col.db.list("""
-        # select id from cards order by ivl and queue = 2""")
-    # if self.revCount:
-    #     self._resetRev()
-    #     return self._fillRev()
+            selected: int = 2
+            sort_by: str = sortconfig.SORT_OPTIONS.get(selected)[1]
 
+            # working
+            self._revQueue = self.col.db.list(
+                f"""select id from cards where
+                did = {did} and queue = 2 and due <= {self.today}
+                order by {sort_by}
+                limit {lim}"""
+            )
 
-# From: anki.schedv2.py
-def currentRevLimit(sched):
-    d = sched.col.decks.get(sched.col.decks.selected(), default=False)
-    return deckRevLimitSingle(sched, d)
+            # THIS ONE WORKS
+            # self._revQueue = self.col.db.list(
+            #     f"""select id from cards where
+            #     did = ? and queue = 2 and due <= ?
+            #     order by lapses asc, ivl desc
+            #     limit ?""", did, self.today, lim,
+            # )
 
+            if self._revQueue:
+                # ordering
+                if self.col.decks.get(did)["dyn"]:
+                    # dynamic decks need due order preserved
+                    self._revQueue.reverse()
+                else:
+                    # random order for regular reviews
+                    # r = random.Random()
+                    # r.seed(self.today)
+                    # r.shuffle(self._revQueue)
 
-# From: anki.schedv2.py
-def deckRevLimitSingle(sched, d, parentLimit=None):
-    if not d: return 0  # invalid deck selected?
-    if d['dyn']: return 99999
+                    # CANNOT SORT AFTER DB BECAUSE  self._revQueue
+                    # is full of cid not card objects.
+                    pass
 
-    c = sched.col.decks.confForDid(d['id'])
-    lim = max(0, c['rev']['perDay'] - d['revToday'][1])
-    if parentLimit is not None:
-        return min(parentLimit, lim)
-    elif '::' not in d['name']:
-        return lim
-    else:
-        for parent in sched.col.decks.parents(d['id']):
-            # pass in dummy parentLimit so we don't do parent lookup again
-            lim = min(lim, deckRevLimitSingle(sched, parent, parentLimit=lim))
-        return lim
+                # is the current did empty?
+                if len(self._revQueue) < lim:
+                    self._revDids.pop(0)
+                return True
+        # nothing left in the deck; move to next
+        self._revDids.pop(0)
 
-
-def checked(self):
-    pass
+    # if we didn't get a card but the count is non-zero,
+    # we need to check again for any cards that were
+    # removed from the queue but not buried
+    if recursing:
+        print("bug: fillRev()")
+        return False
+    self._resetRev()
+    return self._fillRev(recursing=True)
 
 
 def load_config(self):
@@ -114,14 +110,10 @@ def save_config(self):
     addon_config['toggleAddon'] = f.toggleAddon.isChecked()
 
 
-# Hooks
+# # Hooks
 dconf.Ui_Dialog.setupUi = wrap(dconf.Ui_Dialog.setupUi, setup_ui)
 DeckConf.loadConf = wrap(DeckConf.loadConf, load_config)
 DeckConf.saveConf = wrap(DeckConf.saveConf, save_config, 'before')
 
-anki.sched.Scheduler._fillRev = wrap(
-    anki.sched.Scheduler._fillRev, fill_review, 'around'
-)
-# anki.sched.Scheduler._resetRevCount = wrap(
-#     anki.sched.Scheduler._resetRevCount, resetRevCount, 'around'
-# )
+# anki.sched.Scheduler._fillRev = fill_review
+anki.sched.Scheduler._fillRev = wrap(anki.sched.Scheduler._fillRev, fill_review, 'before')
