91afb42a4aeba13002442c5e1f9ad1158a4d525d
mrevanisworking <evanjoshbui@gmail.com>
2020-10-09 14:23:09 -0700

    custom sorting of card queue working

---
+++ src/sortconfig.py
@@ -0,0 +1,15 @@
+SORT_OPTIONS = {
+    0: [
+        "Winner's Queue: Lapses asc, Repetitions desc, Interval desc",
+        "lapses asc, reps desc, ivl desc"
+    ],
+    1: [
+        "Lapses asc, Interval desc",
+        "lapses asc, ivl desc"
+    ],
+    2: [
+        "Loser's Queue: Lapses desc, Repetitions asc, Interval asc",
+        "lapses desc, reps asc, ivl asc"
+    ]
+
+}
